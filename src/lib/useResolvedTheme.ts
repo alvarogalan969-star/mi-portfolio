a9e09a39f4f50e4542001ba59b22fba8933ea913
Alvaro~ <alvarogalan969@gmail.com>
2025-08-25 23:20:20 +0200

    cambio_logo_v1

--- src/lib/useResolvedTheme.ts
+++ src/lib/useResolvedTheme.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 
 type ThemeChoice = "light" | "dark" | "system";
 type Resolved = "light" | "dark";
